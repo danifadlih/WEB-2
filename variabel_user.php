8e0c67eff749a2b2bea2628a2f726f6e46cd825e
danifadlih <danifadlih@gmail.com>
2025-02-21 23:01:05 +0700

    Add files via upload

---
+++ variabel_user.php
@@ -0,0 +1,11 @@
+<?php
+// mendefinisikan variabel 
+$nama = 'Dani fadli h';
+$umur = 19;
+$berat = 60.60;
+
+echo 'nama saya'. $nama;
+echo '<br/>umur:' .$umur . 'Tahun';
+echo '<br/>berat:'. $berat;
+
+?>
